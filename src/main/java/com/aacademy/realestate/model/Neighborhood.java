ba9fc3d9c07bbbfc38fd81c19e35829c34197850
martinasl <martina.slavova@abv.bg>
2021-12-08 03:59:33 +0200

    Add floor and neighborhood entity

---
+++ src/main/java/com/aacademy/realestate/model/Neighborhood.java
@@ -0,0 +1,25 @@
+package com.aacademy.realestate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name = "neighborhood")
+public class Neighborhood {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true, nullable = false)
+    private String name;
+}
